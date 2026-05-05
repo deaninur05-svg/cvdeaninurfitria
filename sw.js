b132fa3809c43dee80d9fc469d52b4552a8a04cd
deaninur05-svg <deaninur05@gmail.com>
2026-05-05 14:20:45 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "cv-cache-v1";
+
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/style.css",
+    "/script.js",
+    "/home.html",
+    "/about.html"
+];
+
+
+self.addEventListener("install", function(event){
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+            .then(function(cache){
+
+                return cache.addAll(urlsToCache);
+
+            })
+
+    );
+
+});
+
+
+self.addEventListener("fetch", function(event){
+
+    event.respondWith(
+
+        caches.match(event.request)
+
+            .then(function(response){
+
+                return response || fetch(event.request);
+
+            })
+
+    );
+
+});
